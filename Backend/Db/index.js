c06ca0dc1ec9e9a0bf8316c893453f0797ad6c76
amey <2022bit153@sggs.ac.in>
2025-04-24 17:21:52 +0530

    up

--- Backend/Db/index.js
+++ Backend/Db/index.js
@@ -1,16 +1,36 @@
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import { DB_NAME } from "../constants.js"
+// import mongoose from "mongoose"
+// import dotenv from "dotenv"
+// import { DB_NAME } from "../constants.js"
 
 
-dotenv.config()
-const connectDB = async()=>{
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log("Mongoose is connected");
-    } catch (error) {
-        console.log("mongoDB connection error :"+ error);
-    }
-}
+// dotenv.config()
+// const connectDB = async()=>{
+//     try {
+//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         console.log("Mongoose is connected");
+//     } catch (error) {
+//         console.log("mongoDB connection error :"+ error);
+//     }
+// }
+
+// export default connectDB;
+
+
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { DB_NAME } from "../constants.js";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    // Ensure MONGODB_URI and DB_NAME are used correctly
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log("Mongoose is connected");
+  } catch (error) {
+    console.log("MongoDB connection error: " + error);
+  }
+};
 
 export default connectDB;
--- Backend/index.js
+++ Backend/index.js
@@ -1,43 +1,92 @@
+// import express from 'express';
+// import cors from 'cors'
+// import cookieParser from 'cookie-parser';
+
+// import { route } from './Routes/route.js';
+// import 'dotenv/config'
+// import connectDB from './Db/index.js';
+
+
+// const server = express();
+// const PORT = process.env.PORT || 8000;
+// server.use(cors({
+//   origin: [process.env.CORS_ORIGIN],
+//   credentials: true, //requiured for setting cookie... /** https://www.reddit.com/r/reactjs/comments/vxvdib/cookie_not_being_set_in_react_app_express_backend/?rdt=46764 */
+// }));
+
+// server.use(cookieParser());
+// server.use(express.json({limit: "16kb"}));
+// server.use(express.urlencoded({ extended: true }));
+
+
+// // connect to db:
+// connectDB()
+// .then(()=>{
+//   // starting the server 
+//     server.listen(PORT , ()=>{
+//         console.log("Server is running at " + PORT);
+//     })
+// })
+// .catch(error=>{
+//     console.log("Error connecting::index.js",error);
+// })
+
+
+// server.use("/api", route)
+
+// server.get("/", (req, res) => {
+//   res.send("Hello to backend")
+// })
+
+// server.get('*', (req, res) => {
+//   res.send("404 NOT FOUND <a href='./'> Go To Home</a>")
+// })
+
+
+
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 import { route } from './Routes/route.js';
-import 'dotenv/config'
+import 'dotenv/config';
 import connectDB from './Db/index.js';
-
 
 const server = express();
 const PORT = process.env.PORT || 8000;
+
+// Update CORS to handle multiple origins if necessary
 server.use(cors({
-  origin: [process.env.CORS_ORIGIN],
-  credentials: true, //requiured for setting cookie... /** https://www.reddit.com/r/reactjs/comments/vxvdib/cookie_not_being_set_in_react_app_express_backend/?rdt=46764 */
+  origin: [process.env.CORS_ORIGIN], // Allow the frontend to connect from this origin
+  credentials: true, // Required for setting cookies (if using cookies for sessions)
 }));
 
 server.use(cookieParser());
-server.use(express.json({limit: "16kb"}));
+server.use(express.json({ limit: "16kb" }));
 server.use(express.urlencoded({ extended: true }));
 
-
-// connect to db:
+// Connect to database:
 connectDB()
-.then(()=>{
-  // starting the server 
-    server.listen(PORT , ()=>{
-        console.log("Server is running at " + PORT);
-    })
-})
-.catch(error=>{
-    console.log("Error connecting::index.js",error);
-})
+  .then(() => {
+    // Start the server after DB connection is successful
+    server.listen(PORT, () => {
+      console.log("Server is running at " + PORT);
+    });
+  })
+  .catch(error => {
+    console.log("Error connecting::index.js", error);
+  });
 
+// Set up the routes
+server.use("/api", route);
 
-server.use("/api", route)
-
+// Default route for the backend (optional)
 server.get("/", (req, res) => {
-  res.send("Hello to backend")
-})
+  res.send("Hello to backend");
+});
 
+// Handle 404 routes
 server.get('*', (req, res) => {
-  res.send("404 NOT FOUND <a href='./'> Go To Home</a>")
-})
+  res.status(404).send("404 NOT FOUND <a href='./'> Go To Home</a>");
+});
+
