36c2d92f1ba8801d81a7c1a687be9ec2025d2568
amey <2022bit153@sggs.ac.in>
2025-04-24 17:16:03 +0530

    up

--- Frontend/src/utils/baseurl.js
+++ Frontend/src/utils/baseurl.js
@@ -1,2 +1,6 @@
-const baseUrl = "http://localhost:8000/api"
-export default baseUrl;
+// const baseUrl = "http://localhost:8000/api"
+// export default baseUrl;
+
+
+const baseurl = "https://mern-project-backend-pink.vercel.app";
+export default baseurl;
